07dd67c42187344455a9d3bcc32d16671b5095ad
ajay-abrol2 <85120431+ajay-abrol2@users.noreply.github.com>
2026-09-18 12:53:44 +0530

    Update default Vertica password in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -78,7 +78,7 @@ def vertica_target():
         'threads': 1,
         'host': os.getenv('VERTICA_HOST', 'verticadb-sample'),
         'username': os.getenv('VERTICA_USER', 'dbadmin'),
-        'password': os.getenv('VERTICA_PASSWORD', ''),
+        'password': os.getenv('VERTICA_PASSWORD', 'Vertica@123'),
         'database': os.getenv('VERTICA_DATABASE', 'vdb'),
         'port': int(os.getenv('VERTICA_PORT', '5433')),
     }
