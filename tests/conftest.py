70a846c4986777ae64cc2fa0cd152b3eaaf96f75
ajay-abrol2 <85120431+ajay-abrol2@users.noreply.github.com>
2026-09-18 09:08:54 +0530

    Update Vertica password to use secrets

--- tests/conftest.py
+++ tests/conftest.py
@@ -42,7 +42,7 @@ def dbt_profile_target():
         'threads': 1,
         'host': 'verticadb-sample',
         'username': 'dbadmin',
-        'password': '',
+        'password': '${{ secrets.VERTICA_PASSWORD }}',
         'database': 'vdb',
         'port': 5433,        
     }
